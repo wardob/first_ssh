89e6f3ebbaac70c29ca720426e5470567502dabf
wardob <wardob@gmail.com>
2017-03-13 15:23:06 +0300

    Update first_test.py

--- first_test.py
+++ first_test.py
@@ -34,23 +34,5 @@ def get_args():
 if __name__ == '__main__':
     args = get_args()
     main(args.host, args.port, args.user, args.password)
-=======
-host = ‘1.1.3.1'
-user = ‘user’
-password = ‘secret’
-port = 22
 
-client = paramiko.SSHClient()
-client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
-client.connect(hostname=host, username=user, password=secret, port=port)
-stdin, stdout, stderr = client.exec_command('terminal length 0\n')
-stdin.write('show ip int br')
-stdin.write('\n')
-stdin.flush()
-output = stdout.readlines()
-print (output)
-#stdin, stdout, stderr = client.exec_command('show int status\n')
-#stdin, stdout, stderr = client.exec_command('show running-config\n')
-output = stdout.readlines()
-client.close()
 

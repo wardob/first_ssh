8dda445f01e5b6bc308070197cc8c16fb58564d8
Nikolay Sedelnikov <n.sedelnikov@rambler-co.ru>
2017-03-13 14:55:53 +0300

    added argparse

--- first_test.py
+++ first_test.py
@@ -1,20 +1,35 @@
+import argparse
 import paramiko
 
-host = '1.1.3.1'
-user = 'user'
-secret = 'secret'
-port = 22
 
-client = paramiko.SSHClient()
-client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
-client.connect(hostname=host, username=user, password=secret, port=port)
-stdin, stdout, stderr = client.exec_command('terminal length 0\n')
-stdin.write('show ip int br')
-stdin.write('\n')
-stdin.flush()
-output = stdout.readlines()
-print (output)
-#stdin, stdout, stderr = client.exec_command('show int status\n')
-#stdin, stdout, stderr = client.exec_command('show running-config\n')
-output = stdout.readlines()
-client.close()
+def main(host, port, user, secret):
+    client = paramiko.SSHClient()
+    client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+    client.connect(hostname=host, username=user, password=secret, port=port)
+    stdin, stdout, stderr = client.exec_command('terminal length 0\n')
+    stdin.write('show ip int br')
+    stdin.write('\n')
+    stdin.flush()
+    output = stdout.readlines()
+    print (output)
+    #stdin, stdout, stderr = client.exec_command('show int status\n')
+    #stdin, stdout, stderr = client.exec_command('show running-config\n')
+    output = stdout.readlines()
+    client.close()
+
+
+def get_args():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("host")
+    parser.add_argument("port")
+    parser.add_argument("user")
+    parser.add_argument("password")
+
+    args = parser.parse_args()
+
+    return args
+
+
+if __name__ == '__main__':
+    args = get_args()
+    main(args.host, args.port, args.user, args.password)
